7f197ccb640c61e2a27266dd5b2e2ea8924a65b1
johnmarsden24 <john.marsden24@gmail.com>
2022-12-01 17:04:18 +0000

    testing

--- .github/actions/greeting/dist/index.js
+++ .github/actions/greeting/dist/index.js
@@ -9690,11 +9690,15 @@ const github = __nccwpck_require__(5438);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const token = core.getInput('token');
+  console.log('//////////////////////////');
+  console.log(token);
+  console.log('//////////////////////////');
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
