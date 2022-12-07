b8b0fa32b2c8f657d4e49822473d650807b811cb
johnmarsden24 <john.marsden24@gmail.com>
2022-12-07 11:30:54 +0000

    updated script

--- .github/actions/generate-release-page/index.js
+++ .github/actions/generate-release-page/index.js
@@ -3,43 +3,53 @@ const { context } = require('@actions/github');
 const { Octokit } = require('octokit');
 
 const createReleasePage = async () => {
+  const repo = 'hello-world-javascript-action';
+  const owner = 'johnmarsden24';
   const token = core.getInput('token');
+  const packageName = core.getInput('package-name');
   const octokit = new Octokit({ auth: token });
 
   const data = await octokit.graphql(
     `
-    query Jonny($name: String!, $owner: String!) {
-      repository(name: $name, owner: $owner) {
-        refs(refPrefix: "refs/tags/", first: 2, query: "prod-test", orderBy: {
+    query GetPreviousTag($repo: String!, $owner: String!, $packageName: String!) {
+      repository(name: $repo, owner: $owner) {
+        refs(refPrefix: "refs/tags/", first: 1, query: $packageName, orderBy: {
         field: TAG_COMMIT_DATE,
         direction: DESC
         }) {
-        totalCount
           nodes {
             name
-            target {
-              oid
-              commitResourcePath
-              abbreviatedOid
-            }
           }
         }
       }
     }
   `,
     {
-      name: 'hello-world-javascript-action',
-      owner: 'johnmarsden24',
+      repo,
+      owner,
+      packageName,
     }
   );
 
-  console.log(JSON.stringify(context, null, 4));
+  const previousTag = repository.refs.nodes[0]?.name;
 
-  return data;
+  const shortSha = context.sha.slice(0, 7);
+  const newTag = `${packageName}-release-${shortSha}`;
+
+  await octokit.request(
+    `POST /repos/${owner}/${repo}/releases/generate-notes`,
+    {
+      owner,
+      repo,
+      tag_name: newTag,
+      target_commitish: 'main',
+      ...(previousTag & { previous_tag_name: previousTag }),
+    }
+  );
 };
 
 createReleasePage()
-  .then((data) => console.log(JSON.stringify(data, null, 4)))
+  .then(() => console.log('success!'))
   .catch((err) => {
     console.log(err.message);
     core.setFailed(err.message);
